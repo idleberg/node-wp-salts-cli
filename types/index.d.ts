02d2482d65f5995ef87009f697a17a04e07425c1
idleberg <git@idleberg.com>
2021-01-27 17:11:18 +0100

    add type interfaces

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,3 +2,18 @@ declare module '*.json' {
   const value: any;
   export default value;
 }
+
+interface WordpressSalts {
+  AUTH_KEY: string;
+  SECURE_AUTH_KEY: string;
+  LOGGED_IN_KEY: string;
+  NONCE_KEY: string;
+  AUTH_SALT: string;
+  SECURE_AUTH_SALT: string;
+  LOGGED_IN_SALT: string;
+  NONCE_SALT: string;
+}
+
+interface CustomSalts extends WordpressSalts {
+  [key: string]: string;
+}
