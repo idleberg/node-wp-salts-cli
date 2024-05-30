9dbf7c61f3617253dad68a23a8e079066defdb3b
Jan T. Sott <git@idleberg.com>
2024-05-30 22:48:08 +0200

    update dependencies, tooling

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,25 @@
+/* eslint-disable */
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:json/recommended'
-  ],
-  overrides: [
-    {
-      files: ['tsconfig.json'],
-      rules: {
-        'json/*': ['error', 'allowComments']
-      }
-    }
-  ]
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint', 'unicorn'],
+	extends: ['eslint:recommended', 'plugin:jsonc/recommended-with-json', 'plugin:@typescript-eslint/recommended'],
+	globals: {
+		Deno: 'readonly',
+	},
+	rules: {
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'unicorn/filename-case': 'error',
+		'unicorn/new-for-builtins': 'error',
+		'unicorn/prefer-node-protocol': 'error',
+	},
+	overrides: [
+		{
+			files: ['tsconfig.json'],
+			rules: {
+				'jsonc/no-comments': 'off'
+			}
+		},
+	],
 };
